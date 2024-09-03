276d6db1bb5cbb9e3a556871b989939c1e0024db
patrikkusnir1 <patrikkusnir1@gmail.com>
2024-09-03 16:16:58 +0300

    greyscale hovering effect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
+
 // attach event listener to the button
 button.addEventListener("click", () => {
   const gridValue = parseInt(window.prompt("Choose your grid size, maximum 100 squares"));
@@ -25,8 +26,31 @@ function createGrid(gridValue) {
     const div = document.createElement("div");
     div.style.width = 100 / gridValue + "%";
     div.style.height = 100 / gridValue + "%";
+    div.style.backgroundColor = "#654565";
+    div.style.transition = "background-color 0.2s";
+
+    // initialize opacity
+    div.dataset.opacity = "0";
+    
+
+
+    // mouseevent for darkening
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "#333";
+      // increased opacity by 10%
+      let opacity = parseFloat(div.dataset.opacity);
+      if (opacity < 1) {
+        opacity = Math.min(opacity + 0.1, 1);
+        div.dataset.opacity = opacity;
+      }
+        // calculate darkening color 
+      const colorValue = Math.floor(255 * (1 - opacity));
+      div.style.backgroundColor = `rgb(${colorValue} ${colorValue} ${colorValue})`
+      if (!div.style.opacity) div.style.opacity = "0";
+      
+      div.style.opacity = opacity + 0.10;
+    })
+    div.addEventListener("click", () => {
+      div.style.backgroundColor = "#125614"
     })
     container.appendChild(div)
   }
